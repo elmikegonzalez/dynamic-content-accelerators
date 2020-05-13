60aebd481ff28d77b0c58e559370ba8672eedfd9
Miguel Gonzalez <mgonzalez@amplience.com>
2020-05-13 15:01:40 -0700

    Update gulpfile.js
    Added gulp-s3 to:
    upload content types to s3
    upload icons to s3
    output hbs to dist from templates

--- gulpfile.js
+++ gulpfile.js
@@ -13,198 +13,238 @@ var addSrc = require('gulp-add-src');
 var connect = require('gulp-connect');
 var flatten = require('gulp-flatten');
 var cleanCSS = require('gulp-clean-css');
+var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('private/presales.json'));
+var s3 = require('gulp-s3-upload')(config);
+// var name;
 
 var toReplace = require('./.replace.json');
 
-var replace = function () {
-    return es.map(function (file, cb) {
-        var fileContent = file.contents.toString();
-        fileContent = fileContent.replace(/\{CONTENT_TYPE_BASEPATH\}/g, toReplace.CONTENT_TYPE_BASEPATH);
-        file.contents = new Buffer.from(fileContent);
-        // send the updated file down the pipe
-        cb(null, file);
-    });
-}
-
-var replaceCompany = function () {
-    return es.map(function (file, cb) {
-        var fileContent = file.contents.toString();
-        fileContent = fileContent.replace(/\{COMPANY_TAG\}/g, toReplace.COMPANY_TAG);
-        file.contents = new Buffer.from(fileContent);
-        // send the updated file down the pipe
-        cb(null, file);
-    });
+var replace = function() {
+  return es.map(function(file, cb) {
+    var fileContent = file.contents.toString();
+    fileContent = fileContent.replace(/\{CONTENT_TYPE_BASEPATH\}/g, toReplace.CONTENT_TYPE_BASEPATH);
+    file.contents = new Buffer.from(fileContent);
+    // send the updated file down the pipe
+    cb(null, file);
+  });
 };
 
-gulp.task('del', function () {
-    return del('dist');
+var replaceCompany = function() {
+  return es.map(function(file, cb) {
+    var fileContent = file.contents.toString();
+    fileContent = fileContent.replace(/\{COMPANY_TAG\}/g, toReplace.COMPANY_TAG);
+    file.contents = new Buffer.from(fileContent);
+    // send the updated file down the pipe
+    cb(null, file);
+  });
+};
+
+gulp.task('del', function() {
+  return del('dist');
 });
 
-gulp.task('copy-icons', function () {
-    return gulp
-        .src([
-            'src/**/*.icon.png'
-        ])
-        .pipe(flatten())
-        .pipe(gulp.dest('dist/icons'));
+gulp.task('copy-icons', function() {
+  return gulp
+    .src([
+      'src/**/*.icon.png'
+    ])
+    .pipe(flatten())
+    .pipe(gulp.dest('dist/icons'));
 });
 
-gulp.task('copy-templates', function () {
-    return gulp
-        .src([
-            'src/*/templates/*.html'
-        ])
-        .pipe(flatten())
-        .pipe(gulp.dest('dist/templates'));
+gulp.task('copy-templates', function() {
+  return gulp
+    .src([
+      'src/*/templates/*.html'
+    ])
+    .pipe(flatten())
+    .pipe(gulp.dest('dist/templates'));
 });
 
-gulp.task('copy-local-content-types', function () {
-    return gulp
-        .src([
-            'src/*/contentTypes/*.json'
-        ])
-        .pipe(replace())
-        .pipe(flatten())
-        .pipe(gulp.dest('dist/contentTypes'));
+gulp.task('copy-templates-to-hbs', function() {
+  return gulp
+    .src(['src/*/templates/*.html'])
+    .pipe(flatten())
+    .pipe(rename(function(path) {
+      path.extname = '.hbs';
+    }))
+    .pipe(gulp.dest('./dist/hbs'));
 });
 
-gulp.task('copy-node-modules', function () {
-    return gulp
-        .src([
-            'node_modules/lory.js/dist/lory.min.js'
-        ])
-        .pipe(gulp.dest('dist'));
+gulp.task('copy-local-content-types', function() {
+  return gulp
+    .src([
+      'src/*/contentTypes/*.json'
+    ])
+    .pipe(replace())
+    .pipe(flatten())
+    .pipe(gulp.dest('dist/contentTypes'));
+});
+
+gulp.task('copy-node-modules', function() {
+  return gulp
+    .src([
+      'node_modules/lory.js/dist/lory.min.js'
+    ])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('upload-content-types', async function() {
+  gulp.src('./dist/contentTypes/**')
+    .pipe(s3({
+        Bucket: 'presalesadisws/demo/maic/baja-reloaded/dc-accelerators/dist/contentTypes', //  Required
+        ACL: 'public-read'       //  Needs to be user-defined
+      },
+      {
+        // S3 Constructor Options, ie:
+        maxRetries: 5
+      }));
+});
+
+gulp.task('upload-icons', async function() {
+  gulp.src('./dist/icons/**')
+    .pipe(s3({
+        Bucket: 'presalesadisws/demo/maic/baja-reloaded/dc-accelerators/dist/icons', //  Required
+        ACL: 'public-read'       //  Needs to be user-defined
+      },
+      {
+        // S3 Constructor Options, ie:
+        maxRetries: 5
+      }));
 });
 
 
-gulp.task('addLoryLicense', function () {
-    return gulp
-        .src('node_modules/lory.js/LICENSE')
-        .pipe(insert.prepend('/*'))
-        .pipe(insert.append('*/'))
-        .pipe(addSrc.append('dist/lory.min.js'))
-        .pipe(concat('lory.min.js'))
-        .pipe(gulp.dest('dist'));
+gulp.task('addLoryLicense', function() {
+  return gulp
+    .src('node_modules/lory.js/LICENSE')
+    .pipe(insert.prepend('/*'))
+    .pipe(insert.append('*/'))
+    .pipe(addSrc.append('dist/lory.min.js'))
+    .pipe(concat('lory.min.js'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-js', function () {
-    return gulp.src([
-            'src/**/*.js',
-            'node_modules/poi-js-lib/dist/poi-lib.min.js',
-            '!**/*.stories.js'
-        ])
-        .pipe(concat('utils.js'))
-        .pipe(replaceCompany())
-        .pipe(gulp.dest('dist'));
+gulp.task('build-js', function() {
+  return gulp.src([
+    'src/**/*.js',
+    'node_modules/poi-js-lib/dist/poi-lib.min.js',
+    '!**/*.stories.js'
+  ])
+    .pipe(concat('utils.js'))
+    .pipe(replaceCompany())
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-js', function () {
-    return gulp.src(['dist/utils.js'])
-        .pipe(uglify())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist'));
+gulp.task('minify-js', function() {
+  return gulp.src(['dist/utils.js'])
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-css', function () {
-    return gulp.src([
-            'src/**/*.scss',
-            '!src/cardsPreview/cardsPreview.scss',
-            '!src/cardsPreview/localCardsStyles.scss'
-        ])
-        .pipe(
-            sass({
-                outputStyle: 'expanded'
-            }).on('error', sass.logError)
-        )
-        .pipe(
-            autoprefixer({
-                browsers: ['last 2 versions'],
-                cascade: false
-            })
-        )
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest('dist'));
+gulp.task('build-css', function() {
+  return gulp.src([
+    'src/**/*.scss',
+    '!src/cardsPreview/cardsPreview.scss',
+    '!src/cardsPreview/localCardsStyles.scss'
+  ])
+    .pipe(
+      sass({
+        outputStyle: 'expanded'
+      }).on('error', sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-cards-css', function () {
-    return gulp.src([
-            'src/cardsPreview/cardsPreview.scss'
-        ])
-        .pipe(
-            sass({
-                outputStyle: 'expanded'
-            }).on('error', sass.logError)
-        )
-        .pipe(
-            autoprefixer({
-                browsers: ['last 2 versions'],
-                cascade: false
-            })
-        )
-        .pipe(concat('cardsStyles.css'))
-        .pipe(gulp.dest('dist'));
+gulp.task('build-cards-css', function() {
+  return gulp.src([
+    'src/cardsPreview/cardsPreview.scss'
+  ])
+    .pipe(
+      sass({
+        outputStyle: 'expanded'
+      }).on('error', sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(concat('cardsStyles.css'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-css', function () {
-    return gulp.src([
-          'dist/styles.css',
-          'dist/cardsStyles.css'
-        ])
-        .pipe(cleanCSS())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist'));
+gulp.task('minify-css', function() {
+  return gulp.src([
+    'dist/styles.css',
+    'dist/cardsStyles.css'
+  ])
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task(
-    'build',
-    gulp.series(
-        'del',
-        'copy-node-modules',
-        'copy-icons',
-        'copy-templates',
-        'copy-local-content-types',
-        'addLoryLicense',
-        'build-js',
-        'minify-js',
-        'build-cards-css',
-        'build-css',
-        'minify-css'
-    ),
-    function () {}
+  'build',
+  gulp.series(
+    'del',
+    'copy-node-modules',
+    'copy-icons',
+    'copy-templates',
+    'copy-templates-to-hbs',
+    'copy-local-content-types',
+    'addLoryLicense',
+    'build-js',
+    'minify-js',
+    'build-cards-css',
+    'build-css',
+    'minify-css'
+  ),
+  function() {
+  }
 );
 
 gulp.task('buildAllWithoutReload', gulp.series('build'));
 
-gulp.task('buildAll', gulp.series('buildAllWithoutReload'), function () {
-    return gulp.src('*').pipe(connect.reload());
+gulp.task('buildAll', gulp.series('buildAllWithoutReload'), function() {
+  return gulp.src('*').pipe(connect.reload());
 });
 
-gulp.task('server', function (done) {
-    connect.server({
-        port: 9100,
-        hostname: '0.0.0.0',
-        livereload: true,
-        debug: true
-    });
+gulp.task('server', function(done) {
+  connect.server({
+    port: 9100,
+    hostname: '0.0.0.0',
+    livereload: true,
+    debug: true
+  });
 
-    done();
+  done();
 });
 
 gulp.task(
-    'buildAllMin',
-    gulp.series('build', 'server'),
-    function () {
-        return gulp.src('*').pipe(connect.reload());
-    }
+  'buildAllMin',
+  gulp.series('build', 'server'),
+  function() {
+    return gulp.src('*').pipe(connect.reload());
+  }
 );
 
-gulp.task('watch', function (done) {
-    gulp.watch('src/**/*', gulp.series(['buildAll']));
-    done();
+gulp.task('watch', function(done) {
+  gulp.watch('src/**/*', gulp.series(['buildAll']));
+  done();
 });
 
 gulp.task('default', gulp.series('buildAll', gulp.parallel('watch', 'server')));
